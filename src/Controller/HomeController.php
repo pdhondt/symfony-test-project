8d1bf9c4745fbd14d4c0e8b3499599085040fe65
Peter <dhondt.peter@gmail.com>
2021-04-26 17:08:37 +0200

    added rest of project files

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'home')]
+    public function index(): Response
+    {
+        $name = 'Peter';
+        $gender = 'M';
+        $hobbies = ['Cycling', 'Snowboarding', 'Coding'];
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'name' => $name,
+            'gender' => $gender,
+            'hobbies' => $hobbies
+        ]);
+    }
+}
